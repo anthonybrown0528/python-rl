d2305d01abc4d23ca7ba5093a50507c38bffac5f
anthonybrown0528 <anthonybrown0528@protonmail.com>
2022-12-29 14:12:34 -0500

    Combine training and evaluation into one script

--- snake-pygame/run_agent.py
+++ snake-pygame/run_agent.py
@@ -1,11 +1,60 @@
+import sys
+import argparse
+import numpy as np
+
 from machine_learning.agent import Agent
 from game import SnakeGameAI
 
-import numpy as np
+def train(model_path_name):
+    plot_scores = []
+    plot_mean_scores = []
+    plot_cost = []
 
-import sys
+    total_score = 0
+    record = 0
 
-def execute(model_filename):
+    agent = Agent([11, 1024, 3])
+    game = SnakeGameAI()
+
+    while True:
+        # get old state
+        state_old = game.get_state()
+
+        # get move
+        final_move = agent.get_action(state_old)
+
+        # perform move and get new state
+        reward, done, score = game.play_step(final_move)
+        state_new = game.get_state()
+
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            # train long memory, plot results
+            game.reset()
+
+            agent.n_games += 1
+            agent.network.epsilon -= 1
+
+            cost = agent.train_long_memory()
+            plot_cost.append(cost)
+
+            if score > record:
+                record = score
+                agent.network.save(model_path_name)
+
+            print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+
+def evauluate(model_filename):
     agent = Agent([11, 1024, 3], eval=True)
     game = SnakeGameAI()
 
@@ -41,10 +90,23 @@ def execute(model_filename):
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
 if __name__ == '__main__':
-    model_path = 'tmp.pth'
 
-    if len(sys.argv) == 2:
-        model_path = sys.argv[1]
+    # Parse CLI arguments
+
+    parser = argparse.ArgumentParser(description='Trains/evaluates a learning model playing the Snake game')
+
+    parser.add_argument('-f', '--filename', default='tmp.pth', help='An optional argument for specifying file name of the model to store or load')
+    parser.add_argument('-e', '--eval', action='store_true', help='An optional argument for specifying the program to run in evaluation mode instead of training mode')
+
+    args = parser.parse_args()
+
+    # Run program
+
+    model_path = args.filename
     
-    print(f'{model_path} loaded as model')
-    execute(model_path)
+    if args.eval:
+        print(f'{model_path} loaded as model')
+        evauluate(model_path)
+    else:
+        print(f'Model will be saved as {model_path}')
+        train(model_path)
