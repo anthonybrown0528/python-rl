758683b89cfe0df6fa2d90abc5529a1926b099de
anthonybrown0528 <anthonybrown0528@protonmail.com>
2023-01-02 16:14:53 -0500

    implement functions for training and evaluation

--- examples/bipedal_walker/run_agent.py
+++ examples/bipedal_walker/run_agent.py
@@ -1,6 +1,7 @@
 import sys
 import gym
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 sys.path.append('../../..')
@@ -22,57 +23,143 @@ def get_action_from_discrete(discrete_action, discrete_action_space):
 
     return action
 
-def train():
-    num_observation = 24
-    discretization = 5
-    dof = 4
-
+def train(num_observation, discretization, dof, gui=False):
     discrete_action_space = np.linspace([-1.0] * dof, [1.0] * dof, discretization)
 
     # Construct environment
-    env = gym.make('BipedalWalker-v3', render_mode='human')
+    env = None
+    if gui:
+        env = gym.make('BipedalWalker-v3', render_mode='human')
+    else:
+        env = gym.make('BipedalWalker-v3')
 
     # Construct an agent
     agent = Agent([num_observation, 1024, discretization**dof])
-    agent.network.epsilon = 50
+    agent.network.epsilon = 200
 
-    # Run training episodes
-    num_episodes = 300
-    for episode in range(num_episodes):
-        old_state = env.reset()
-        old_state = old_state[0]
+    # Used to store cost over episodes
+    costs = []
 
-        high_score = 0
-        score = 0
+    try:
+        # Run training episodes
+        num_episodes = 300
+        for episode in range(num_episodes):
+            old_state = env.reset()
+            old_state = old_state[0]
 
-        done = False
-        while not done:
-            # Make discrete action from model
-            discrete_action = agent.get_action(old_state)
-            action = get_action_from_discrete(discrete_action, discrete_action_space)
+            score = 0
 
-            new_state, reward, term, trunc, _ = env.step(action=action)
-            score = reward
+            done = False
+            while not done:
+                # Make discrete action from model
+                discrete_action = agent.get_action(old_state)
+                action = get_action_from_discrete(discrete_action, discrete_action_space)
 
-            done = term or trunc
+                new_state, reward, term, trunc, _ = env.step(action=action)
+                score = reward
 
-            # Record in replay buffer
-            agent.remember(old_state, discrete_action, reward, new_state, done)
+                done = term or trunc
 
-            # Visualize the environment
-            env.render()
+                # Record in replay buffer
+                agent.remember(old_state, discrete_action, reward, new_state, done)
 
-        if score > high_score:
-            high_score = score
-            agent.network.save()
+                # Visualize the environment
+                if gui:
+                    env.render()
 
-        # Update weights
-        agent.train_long_memory()
-        print(f'Game {episode} Score: {score}')
+            agent.network.epsilon -= 1
+
+            # Update weights
+            cost = agent.train_long_memory()
+            costs.append(cost)
+
+            print(f'Game {episode}, Score: {score}, Cost: {cost}')
+        
+    except KeyboardInterrupt:
+        pass
+
+    print('Saving model to file...')
+    agent.network.save()
+
+    # Generate graph of model metrics
+    plt.plot(costs)
+    plt.show()
+
+def evaluate(num_observation, discretization, dof, gui=False):
+    discrete_action_space = np.linspace([-1.0] * dof, [1.0] * dof, discretization)
+
+    # Construct environment
+    if gui:
+        env = gym.make('BipedalWalker-v3', render_mode='human')
+    else:
+        env = gym.make('BipedalWalker-v3')
+
+    # Construct an agent
+    agent = Agent([num_observation, 1024, discretization**dof])
+
+    agent.network.load()
+    agent.network.epsilon = 0
+
+    agent.network.model.eval()
+    agent.network.model.zero_grad()
+
+    # Used to store cost over episodes
+    scores = []
+
+    try:
+        # Run training episodes
+        num_episodes = 100
+        for episode in range(num_episodes):
+            old_state = env.reset()
+            old_state = old_state[0]
+
+            score = 0
+
+            done = False
+            while not done:
+                # Make discrete action from model
+                discrete_action = agent.get_action(old_state)
+                action = get_action_from_discrete(discrete_action, discrete_action_space)
+
+                new_state, reward, term, trunc, _ = env.step(action=action)
+                score = reward
+
+                done = term or trunc
+
+                # Record in replay buffer
+                agent.remember(old_state, discrete_action, reward, new_state, done)
+
+                # Visualize the environment
+                if gui:
+                    env.render()
+
+
+            # Update weights
+            cost = agent.train_long_memory()
+            scores.append(score)
+
+            print(f'Game {episode}, Score: {score}, Cost: {cost}')
+        
+    except KeyboardInterrupt:
+        pass
+
+    # Generate graph of model metrics
+    plt.plot(scores)
+    plt.show()
 
 
 def main():
-    train()
+    eval = False
+    with_gui = False
+
+    num_observation = 24
+    discretization = 10
+    dof = 4
+
+    if eval:
+        evaluate(num_observation, discretization, dof, with_gui)
+    else:
+        train(num_observation, discretization, dof, with_gui)
 
 if __name__ == '__main__':
     main()
