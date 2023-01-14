6231c29b260c9055fcd9dcb69044c56087edc788
anthonybrown0528 <anthonybrown0528@protonmail.com>
2023-01-14 15:07:28 -0500

    reduce repetitive code in train() and evaluate()

--- agent.py
+++ agent.py
@@ -6,33 +6,55 @@ import random
 import math
 from collections import namedtuple
 
-import numpy as np
-
-
 from machine_learning.dqn import DQN
 from machine_learning.replay_buffer import ReplayBuffer
 
 class Agent:
+
+    # BATCH_SIZE is the number of transitions sampled from the replay buffer
+    # GAMMA is the discount factor as mentioned in the previous section
+    # EPS_START is the starting value of epsilon
+    # EPS_END is the final value of epsilon
+    # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
+    # TAU is the update rate of the target network
+    # LR is the learning rate of the AdamW optimizer
+    BATCH_SIZE = 128
+    MEMORY_SIZE = 10000
+    GAMMA = 0.99
+    TAU = 0.005
+    LR = 1e-4
+    DEVICE = "cpu"
     
-    def __init__(self, n_observations, n_actions, gamma=0.99, batch_size=128, memory_size=10000, lr=1e-4, device="cpu"):
+    def __init__(self, n_observations, n_actions, gamma=GAMMA, batch_size=BATCH_SIZE, memory_size=MEMORY_SIZE, lr=LR, device=DEVICE):
+        self._n_observations = n_observations
+        self._n_actions = n_actions
+        
         self._device = device
         self._gamma = gamma # discount rate
 
         self._batch_size = batch_size
         self.memory = ReplayBuffer(memory_size)
 
-        self.policy_net = DQN(n_observations, n_actions).to(self._device)
-        self.target_net = DQN(n_observations, n_actions).to(self._device)
+        self.policy_net = DQN(self._n_observations, self._n_actions).to(self._device)
+        self.target_net = DQN(self._n_observations, self._n_actions).to(self._device)
         self.target_net.load_state_dict(self.policy_net.state_dict())
 
         self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=lr, amsgrad=True)
         self.criterion = nn.MSELoss()
+
+        self._steps_done = 0
 
     def set_gamma(self, gamma):
         self._gamma = gamma
 
     def get_gamma(self) -> float:
         return self._gamma
+
+    def set_steps_done(self, steps_done):
+        self._steps_done = steps_done
+
+    def get_steps_done(self) -> int:
+        return self._steps_done
 
     def remember(self, state, action, reward, next_state, done):
         self._memory.add((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
@@ -42,7 +64,7 @@ class Agent:
             return
 
         Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
+                        ('state', 'action', 'next_state', 'reward', 'done'))
 
         transitions = self.memory.sample(self._batch_size)
         # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
@@ -86,8 +108,16 @@ class Agent:
         torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
         self.optimizer.step()
 
+        # Soft update of the target network's weights
+        # θ′ ← τ θ + (1 −τ )θ′
+        target_net_state_dict = self.target_net.state_dict()
+        policy_net_state_dict = self.policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key]*self.TAU + target_net_state_dict[key]*(1-self.TAU)
+        self.target_net.load_state_dict(target_net_state_dict)
+
+
     def select_action(self, state, steps_done, env):
-        # global steps_done
         sample = random.random()
 
         eps_threshold = DQN.EPS_START * math.exp(-steps_done / DQN.EPS_DECAY)
--- examples/cartpole/solution.py
+++ examples/cartpole/solution.py
@@ -1,114 +1,73 @@
 import gym
-import math
-import random
-
 import torch
 
 import sys
 sys.path.append('../../..')
 
-from machine_learning.dqn import DQN
 from machine_learning.agent import Agent
 
 env = gym.make('CartPole-v1')
-# Get number of actions from gym action space
-n_actions = env.action_space.n
-# Get the number of state observations
 state, _ = env.reset()
+
+# Get number of actions and state observations
+n_actions = env.action_space.n
 n_observations = len(state)
 
 # if gpu is to be used
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+agent = Agent(n_observations, n_actions, device=device)
 
-# BATCH_SIZE is the number of transitions sampled from the replay buffer
-# GAMMA is the discount factor as mentioned in the previous section
-# EPS_START is the starting value of epsilon
-# EPS_END is the final value of epsilon
-# EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
-# TAU is the update rate of the target network
-# LR is the learning rate of the AdamW optimizer
-BATCH_SIZE = 128
-MEMORY_SIZE = 10000
-GAMMA = 0.99
-TAU = 0.005
-LR = 1e-4
+def play_episode(eval=False):
+    # Initialize the environment and get it's state
+    state, _ = env.reset()
+    state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
 
-agent = Agent(n_observations, n_actions, GAMMA, BATCH_SIZE, MEMORY_SIZE, LR, device=device)
+    score = 0
+    done = False
+    while not done:
+        action = agent.select_action(state, agent.get_steps_done(), env)
+        observation, reward, terminated, truncated, _ = env.step(action.item())
+
+        score += reward
+
+        reward = torch.tensor([reward], device=device)
+
+        next_state = None if terminated else torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+
+        if not eval:
+
+            # Store the transition in memory
+            agent.memory.push((state, action, next_state, reward, terminated))
+            # Perform one step of the optimization (on the policy network)
+            agent.optimize_model()
+
+        # Move to the next state
+        state = next_state
+
+        done = terminated or truncated
+        agent.set_steps_done(agent.get_steps_done() + 1)
+
+    return score
+
 
 def evaluate(num_episodes):
     agent.policy_net.load('model/model.pth')
-    steps_done = 0
+    
+    for i_episode in range(num_episodes):
 
-    for _ in range(num_episodes):
-        # Initialize the environment and get it's state
-        state, _ = env.reset()
-        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
-        done = False
-        while not done:
-            action = agent.select_action(state, steps_done, env)
-            steps_done += 1
-            observation, reward, terminated, truncated, _ = env.step(action.item())
-            reward = torch.tensor([reward], device=device)
-            done = terminated or truncated
-
-            if terminated:
-                next_state = None
-            else:
-                next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
-
-            # Move to the next state
-            state = next_state
-
-            if done:
-                break
+        score = play_episode(True)
+        print('Game:', i_episode, 'Score:', score)
 
 
     print('Complete')
 
 def train(num_episodes):
-    steps_done = 0
     for i_episode in range(num_episodes):
-        # Initialize the environment and get it's state
-        state, _ = env.reset()
-        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
-        done = False
-        score = 0
-        while not done:
-            action = agent.select_action(state, steps_done, env)
-            steps_done += 1
-            observation, reward, terminated, truncated, _ = env.step(action.item())
-            score += reward
-            reward = torch.tensor([reward], device=device)
-            done = terminated or truncated
 
-            if terminated:
-                next_state = None
-            else:
-                next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
-
-            # Store the transition in memory
-            agent.memory.push((state, action, next_state, reward))
-
-            # Move to the next state
-            state = next_state
-
-            # Perform one step of the optimization (on the policy network)
-            agent.optimize_model()
-
-            # Soft update of the target network's weights
-            # θ′ ← τ θ + (1 −τ )θ′
-            target_net_state_dict = agent.target_net.state_dict()
-            policy_net_state_dict = agent.policy_net.state_dict()
-            for key in policy_net_state_dict:
-                target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
-            agent.target_net.load_state_dict(target_net_state_dict)
-
-            if done:
-                print(f'Game {i_episode} Score: {score + 1}')
-                break
-
+        score = play_episode()
+        print('Game:', i_episode, 'Score:', score)
 
     agent.policy_net.save('model/model.pth')
     print('Complete')
 
-train(300)
+evaluate(300)
